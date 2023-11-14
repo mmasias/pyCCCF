5f4f1c08e2c8582b395fcac282e7bfd95d0046e7
manuel@AIOlinux <manuel@masiasweb.com>
2023-11-14 19:23:56 +0100

    Introducimos métodos estáticos

--- src/vPRG1/CCCF.java
+++ src/vPRG1/CCCF.java
@@ -18,72 +18,83 @@ class CCCF {
         int customerService1 = 0, customerService2 = 0, customerService3 = 0, customerService4 = 0;
         final int ITEM_MINIMUM = 5;
         final int ITEM_MAXIMUM = 15;
-        final int DECIMAL_PLACES = 100;
 
         do {
             currentTime = currentTime + MINUTE;
             isWorking = currentTime < CLOSING_TIME;
 
             line = line + (Math.random() < PROBABILITY_ARRIVAL ? 1 : 0);
-            System.out.println("\nESTADO " + "-=".repeat(10));
-            System.out.println("HORA: " + (double) Math.round(currentTime * DECIMAL_PLACES) / DECIMAL_PLACES);
-            System.out.print("> COLA DE " + line + " | ");
+            showHeader(currentTime, line);
 
-            if (line >0 && customerService1 <= 0) {
+            if (line > 0 && customerService1 <= 0) {
                 line = line - 1;
                 customerService1 = (int) (Math.random() * (ITEM_MAXIMUM - ITEM_MINIMUM)) + ITEM_MINIMUM;
-                System.out.print("Cliente a caja 1, con [" + customerService1 + "] pack de items | ");
-            } 
-            
-            if (line >0 && customerService2 <= 0) {
+                describeArrival(customerService1, 1);
+            }
+
+            if (line > 0 && customerService2 <= 0) {
                 line = line - 1;
                 customerService2 = (int) (Math.random() * (ITEM_MAXIMUM - ITEM_MINIMUM)) + ITEM_MINIMUM;
-                System.out.print("Cliente a caja 2, con [" + customerService2 + "] pack de items | ");
-            } 
-            
-            if (line >0 && customerService3 <= 0) {
+                describeArrival(customerService2, 2);
+            }
+
+            if (line > 0 && customerService3 <= 0) {
                 line = line - 1;
                 customerService3 = (int) (Math.random() * (ITEM_MAXIMUM - ITEM_MINIMUM)) + ITEM_MINIMUM;
-                System.out.print("Cliente a caja 3, con [" + customerService3 + "] pack de items | ");
-            } 
-            
-            if (line >0 && customerService4 <= 0) {
+                describeArrival(customerService3, 3);
+            }
+
+            if (line > 0 && customerService4 <= 0) {
                 line = line - 1;
                 customerService4 = (int) (Math.random() * (ITEM_MAXIMUM - ITEM_MINIMUM)) + ITEM_MINIMUM;
-                System.out.print("Cliente a caja 4, con [" + customerService4 + "] pack de items | ");
+                describeArrival(customerService3, 3);
             }
 
             System.out.println("\nLINEA DE CAJAS " + "-=".repeat(10));
 
             if (customerService1 > 0) {
                 customerService1 = customerService1 - 1;
-                System.out.println("> Caja 1 atiende al cliente. Le quedan [" + customerService1 + "] pack de items.");
-            } else {
-                System.out.println("> Caja 1 libre.");
             }
-            
+            describeCustomerService(customerService1, 1);
+
             if (customerService2 > 0) {
                 customerService2 = customerService2 - 1;
-                System.out.println("> Caja 2 atiende al cliente. Le quedan [" + customerService2 + "] pack de items.");
-            } else {
-                System.out.println("> Caja 2 libre.");
             }
-            
+            describeCustomerService(customerService2, 2);
+
             if (customerService3 > 0) {
                 customerService3 = customerService3 - 1;
-                System.out.println("> Caja 3 atiende al cliente. Le quedan [" + customerService3 + "] pack de items.");
-            } else {
-                System.out.println("> Caja 3 libre.");
             }
-            
+            describeCustomerService(customerService3, 3);
+
             if (customerService4 > 0) {
                 customerService4 = customerService4 - 1;
-                System.out.println("> Caja 4 atiende al cliente. Le quedan [" + customerService4 + "] pack de items.");
-            } else {
-                System.out.println("> Caja 4 libre.");
-            } 
+            }
+            describeCustomerService(customerService4, 4);
 
             String userInput = scanner.nextLine();
         } while (isWorking);
+    }
+
+    private static void describeCustomerService(int customerService, int customerServiceNumber) {
+        if (customerService > 0) {
+            System.out.println(
+                    "> Caja " + customerServiceNumber + " atiende al cliente. Le quedan [" + customerService
+                            + "] pack de items.");
+        } else {
+            System.out.println("> Caja " + customerServiceNumber + " libre.");
+        }
+    }
+
+    private static void describeArrival(int customerService, int customerServiceNumber) {
+        System.out
+                .print("Cliente a caja " + customerServiceNumber + ", con [" + customerService + "] pack de items | ");
+    }
+
+    private static void showHeader(double currentTime, int line) {
+        final int DECIMAL_PLACES = 100;
+        System.out.println("\nESTADO " + "-=".repeat(10));
+        System.out.println("HORA: " + (double) Math.round(currentTime * DECIMAL_PLACES) / DECIMAL_PLACES);
+        System.out.print("> COLA DE " + line + " | ");
     }
 }
