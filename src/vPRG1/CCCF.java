d7a8e628acef2d01797658ad7dc7a5ffbace935a
manuel@AIOlinux <manuel@masiasweb.com>
2023-11-14 18:44:08 +0100

    Contamos la historia.

--- src/vPRG1/CCCF.java
+++ src/vPRG1/CCCF.java
@@ -1,9 +1,11 @@
 package vPRG1;
 
-class CCCF {
+import java.util.Scanner;
 
+class CCCF {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         final double OPENING_TIME = 9.0;
         final double CLOSING_TIME = 21.0;
         final double MINUTE = 1.0 / 60.0;
@@ -16,25 +18,28 @@ class CCCF {
         int customerService = 0;
         final int ITEM_MINIMUM = 5;
         final int ITEM_MAXIMUM = 15;
+        final int DECIMAL_PLACES = 100;
 
         do {
             currentTime = currentTime + MINUTE;
             isWorking = currentTime < CLOSING_TIME;
 
             line = line + (Math.random() < PROBABILITY_ARRIVAL ? 1 : 0);
+            System.out.println((double) Math.round(currentTime * DECIMAL_PLACES) / DECIMAL_PLACES);
+            System.out.println("> COLA DE " + line);
 
             if (customerService <= 0) {
                 line = line - 1;
                 customerService = (int) (Math.random() * (ITEM_MAXIMUM - ITEM_MINIMUM)) + ITEM_MINIMUM;
+                System.out.println("> Pasa un cliente a caja 1, con [" + customerService + "] pack de items");
             }
 
             if (customerService > 0) {
                 customerService = customerService - 1;
+                System.out.println("> Caja 1 atiende al cliente. Le quedan [" + customerService + "] pack de items.");
             }
 
-            System.out.println(currentTime);
-            System.out.println("> COLA DE " + line);
-
+            String userInput = scanner.nextLine();
         } while (isWorking);
     }
 }
