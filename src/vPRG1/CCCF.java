561aa737744bf5e93fb3a310f41c458f8cc71dac
manuel@AIOlinux <manuel@masiasweb.com>
2023-11-14 13:16:40 +0100

    Una caja registradora...

--- src/vPRG1/CCCF.java
+++ src/vPRG1/CCCF.java
@@ -13,12 +13,25 @@ class CCCF {
         int line = 0;
         final double PROBABILITY_ARRIVAL = 0.6;
 
+        int customerService = 0;
+        final int ITEM_MINIMUM = 5;
+        final int ITEM_MAXIMUM = 15;
+
         do {
             currentTime = currentTime + MINUTE;
             isWorking = currentTime < CLOSING_TIME;
-            
+
             line = line + (Math.random() < PROBABILITY_ARRIVAL ? 1 : 0);
-            
+
+            if (customerService <= 0) {
+                line = line - 1;
+                customerService = (int) (Math.random() * (ITEM_MAXIMUM - ITEM_MINIMUM)) + ITEM_MINIMUM;
+            }
+
+            if (customerService > 0) {
+                customerService = customerService - 1;
+            }
+
             System.out.println(currentTime);
             System.out.println("> COLA DE " + line);
 
