a172c85f26c97d0e488fb3c0f0af750af153a844
manuel@AIOlinux <manuel@masiasweb.com>
2023-11-14 12:53:51 +0100

    Se incluye la cola...

--- src/vPRG1/CCCF.java
+++ src/vPRG1/CCCF.java
@@ -3,19 +3,25 @@ package vPRG1;
 class CCCF {
 
     public static void main(String[] args) {
-        
+
         final double OPENING_TIME = 9.0;
         final double CLOSING_TIME = 21.0;
-        final double MINUTE = 1.0/60.0;
+        final double MINUTE = 1.0 / 60.0;
         double currentTime = OPENING_TIME;
         boolean isWorking = true;
-        
-        do{
-            currentTime = currentTime + MINUTE;
-            System.out.println(currentTime);
-            isWorking=currentTime<CLOSING_TIME;
-            
-        }while(isWorking);
-    }
 
+        int line = 0;
+        final double PROBABILITY_ARRIVAL = 0.6;
+
+        do {
+            currentTime = currentTime + MINUTE;
+            isWorking = currentTime < CLOSING_TIME;
+            
+            line = line + (Math.random() < PROBABILITY_ARRIVAL ? 1 : 0);
+            
+            System.out.println(currentTime);
+            System.out.println("> COLA DE " + line);
+
+        } while (isWorking);
+    }
 }
